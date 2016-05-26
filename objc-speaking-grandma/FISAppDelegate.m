9c991ae0c3a74c0be09d492bda317172f0c277b8
saraahmad1114 <saraahmad1114@yahoo.com>
2016-05-26 14:14:01 -0400

    did grandma all over again

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,12 +7,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    NSString *talkToGrandma = @"HI, GRANDMA";
+    //Created a string with a greeting to Grandma!
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    //Uppercase version of talkToGrandma variable that points to the variable shoutAtGrandma
+    //Comparison of both strings, the bool should return FALSE or 0
+    //** Thing to remember is when you use the isEqualToString method if you are comparing two strings that are uppercase then they will come out as equal but if they are lowercase and they are compared then the actual words will be compared and if they are the exact same thing. 
     
+    talkToGrandma = @"Hi, Grandma!";
+//    shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    if(!shouting)
+    {
+        NSLog(@"NO, NOT SINCE 1983!");
+    }
+    //the if statement says that if the bool value comes out to true, meaning both the talkToGrandma and shoutAtGrandma are equal then print "NO, NOT SINCE 1983!"
+    else
+    {
+        NSLog(@"WHAT'S THAT? SPEAK UP DEAR!");
+    }
+    //if the contents of the if statement equal false then the default or the else statement will run! 
     return YES; // Don't alter this statement.
 }
 
